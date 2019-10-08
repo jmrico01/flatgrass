1ee8d33a92be87fea5d81a4830f5fc3f16d215b8
Jose M Rico <jrico@princeton.edu>
2019-10-08 19:24:54 -0400

    rm

--- data/other/momsrvr_refs.c
+++
@@ -1,9 +0,0 @@
-#include <sysutil_ident.h>
-SYSUTIL_IDENT_RCSID(momsrvr_refs_c,"$Id: momsrvr_refs.c 539956 2013-09-11 13:36:41Z mramezan $ $CSID: 522FA265034AC11903 $")
-
-
-SYSUTIL_IDENT_RCSID(momsrvr_refs_c,"$Id: ")
-#include <big_refs.h>
-static void x(void) {
-    DR(calcrt2_wrap_)
-} 
